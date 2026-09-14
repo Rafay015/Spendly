3ce0463d6a8554bbe9039026dfcf31bdb46f5252
Rafay Wadood <rafeywadood15@gmail.com>
2026-09-14 18:08:35 +0500

    fixing paddings

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
   };
 
   return (
-    <div className="bg-slate-950 min-h-screen items-center px-5 sm:px-10 lg:px-20 pb-20">
+    <div className="bg-slate-950 min-h-screen items-center px-3 sm:px-6 lg:px-10 pb-20">
       <Header />
       <div className="mt-10 sm:mt-20 lg:mt-0">
         <Details income={income} expense={expense} />
